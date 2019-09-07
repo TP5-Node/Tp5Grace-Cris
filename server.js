4561051f2ccd7d16f2a1f902d11ae1cd6cce4fbe
m.c.gomez@live.com <you@example.com>
2019-09-06 21:03:00 -0300

    add router

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const router = require('./modules/router')
 const server = express();
 const port = 3000;
 
-server.listen(port, ()=>{
+server.use(router);
+server.use(express.static('public'))
 
-    
+
+server.listen(port, ()=>{
+    console.log(`Running on port ${port}`)
+
 }) ;
