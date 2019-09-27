6b4e2bfbafe0d8e0a525b5769b8b231a8562d172
m.c.gomez@live.com <you@example.com>
2019-09-27 17:00:14 -0300

    filter & post finish

--- public/js/main2.js
+++ public/js/main2.js
@@ -44,7 +44,7 @@ const createEditBtn = () =>{
     btn.innerHTML = `<i class="material-icons" title="Edit">&#xE254;</i>`
     btn.href = "#"
     btn.onclick = () =>{
-        showEditModal()
+        patchCourse()
     } 
     return btn
 } 
@@ -57,6 +57,37 @@ const createDelBtn = () =>{
         showDeleteModal()
     } 
     return btn
+}
+//FUNCION DE PATCH
+const patchCourse = () =>{
+    showEditModal()
+    let idRow = document.getElementById()
+    let editName = document.getElementById('editName').value
+    let editModality = document.getElementById('editModality').value
+    let editPrice = document.getElementById('editPrice').value
+    let editEmail = document.getElementById('editEmail').value
+    let idEdit = document.getElementById('editId').innerText
+        
+    fetch('/api/employees', {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(employee)
+    })
+    
+}
+
+//FUNCION PARA DELETEAR
+const deleteCourse = (id) =>{
+    fetch('/api/resources/:id', {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+    })
+        .then(res=>res.json())
+        .then(res=>{
+            console.log(res)
+            closeDeleteModal()
+            initialize()
+        })
 }
 
 
@@ -95,81 +126,108 @@ const closeDeleteModal = () =>{
 
 
 //FUNCION PARA HACER EL POST
-
-
-const createUser = () => {
+const postCourse = () => {
 	event.preventDefault();
     const formName = document.getElementById('name');
     const formModality = document.getElementById('modality');
     const formPrice = document.getElementById('price');
     const formEmail = document.getElementById('email');
-
-	const payload = {
-		name: formName.value,
+    const newCourse = {
+        name: formName.value,
         modality: formModality.value,
         price: formPrice.value,
         email: formEmail.value
     }
-
     fetch('api/resources', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(newCourse)
     })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-            formId.value = '';
-            formName.value = '';
-            initialize();
+    .then((res) => res.json())
+    .then((res) => {
+        console.log(res);
+        formId.value = '';
+        formName.value = '';
+        
+    })
+    initialize();
+    closeModal() 
+    cleanInputs('name')  
+    cleanInputs('modality')  
+    cleanInputs('price')  
+    cleanInputs('email')  
+}
+
+//FUNCION PARA REINICIAR LOS INPUTS 
+const cleanInputs = (inputNod) =>{
+    const cleanNod = document.getElementById(inputNod)
+    cleanNod.value = "" 
+}      
+   
+    // const validationName = () =>{    
+    //     if(name.lenght < 30 && name !== ''){
+    //     return true
+    //     }else{
+    //         return false
+    //     }
+    // }
+    // const validationMod = (modality) =>{    
+    //     if(modality.lenght < 30 && modality !== ''){
+    //     return true
+    //     }else{
+    //         return false
+    //     }
+    // }
+    // const validationPrice = (price) =>{    
+    //     const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    //     return re.test(price)
+    // }
+
+    // const validationEmail = (email) =>{
+    //     const re = /\S+@\S+\.\S+/
+    //     return re.test(email)
+    // }
+
+
+
+
+//FUNCION PARA FILTER
+const filterContent = () =>{
+    fetch('/api/resources')
+    .then(res => res.json())
+    .then(response =>{
+        const data = response.resources
+        let valueInput = document.getElementById('filterInput').value
+        let filterInput = data.filter(resource => Object.keys(resource).find( prop => resource[prop].includes(valueInput)))
+        const list = document.getElementById('tableContain');
+        list.innerHTML = '';
+        filterInput.forEach(elem =>{
+        let rowFilter = document.createElement('tr')
+        let filterName = document.createElement('td')
+        filterName.innerText = elem.name
+        let filterModality = document.createElement('td')
+        filterModality.innerText = elem.modality
+        let filterPrice = document.createElement('td')
+        filterPrice.innerText = elem.price
+        let filterEmail = document.createElement('td')
+        filterEmail.innerText = elem.email
+        rowFilter.appendChild(filterName)
+        rowFilter.appendChild(filterModality)
+        rowFilter.appendChild(filterPrice)
+        rowFilter.appendChild(filterEmail) 
+        let editBtn = document.createElement('td')
+        editBtn.appendChild(createEditBtn())
+        editBtn.appendChild(createDelBtn())
+        rowFilter.appendChild(editBtn)   
+        list.appendChild(rowFilter)
         })
-        .catch((error) => {
-            // acá van otras cositas
-        });
-    } 
-    
-
-const isValid = (payload) => {
-	//acá valido las cositas
-	return true;
-};
-
-
-
-
-    
-// FUNCION PARA OBTENER LOS CAMPOS DEL INPUT
-const loadedInfo = (dataPlace, info) =>{
-const loadedData = document.getElementsByClassName(dataPlace)
-loadedData.value = info
+    })
 }
-
-
-const sendData = () =>{
-if(!name.lenght > 30){
-    loadedInfo(name, 'name')
-}
-
-// VALIDACION DEL MAIL
-emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-if (emailRegex.test(email.value)) {
-    loadedInfo(email, 'email')
-}
-if(!address.lenght > 30){
-    loadedInfo(address, 'address')
-}
-phoneRegex = /^[0-9]$/
-if(phoneRegex.test(phone.value)){
-    loadedInfo(phone, 'phone')
-}
-}
-
-
-
-
-// FUNCION PARA EDITAR
-
-//FUNCION PARA BORRAR
-
+       
+const enterKeyPress = event =>{
+    if( event.code === 'Enter'){ 
+        filterContent()
+    }  
+} 
